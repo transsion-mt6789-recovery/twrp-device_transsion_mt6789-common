279a97e54f8d7ae7d2124ebc30921b7892264858
Rama Bondan Prakoso <ramanarubp@gmail.com>
2023-11-12 05:35:58 +0700

    Infinix-X678B: Add vendorsetup.sh
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+export FOX_AB_DEVICE=1
+export FOX_VIRTUAL_AB_DEVICE=1
+export OF_DEFAULT_KEYMASTER_VERSION=4.1
+export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+export OF_AB_DEVICE_WITH_RECOVERY_PARTITION=1
+export OF_MAINTAINER="rama982"
+export FOX_VARIANT="A13+"
+export OF_FLASHLIGHT_ENABLE=0
+
+export FOX_USE_BASH_SHELL=1
+export FOX_USE_NANO_EDITOR=1
+export FOX_USE_TAR_BINARY=1
+export FOX_USE_SED_BINARY=1
+export FOX_USE_XZ_UTILS=1
+export FOX_ASH_IS_BASH=1
+export OF_ENABLE_LPTOOLS=1
+export FOX_DELETE_MAGISK_ADDON=1
+export FOX_DELETE_AROMAFM=1
+
+export FOX_USE_DATA_RECOVERY_FOR_SETTINGS=1
+
+export OF_LOOP_DEVICE_ERRORS_TO_LOG=1
+
+export OF_USE_LZ4_COMPRESSION=true
+
+export OF_SCREEN_H=2400
+export OF_STATUS_H=95
+export OF_STATUS_INDENT_LEFT=48
+export OF_STATUS_INDENT_RIGHT=48
+export OF_ALLOW_DISABLE_NAVBAR=0
+export OF_CLOCK_POS=1
+
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+export CCACHE_MAXSIZE="5G"
+export CCACHE_DIR="/mnt/ccache"
+
+if [ ! -d ${CCACHE_DIR} ];
+then
+  echo "CCACHE Directory/Partition is not mounted at \"${CCACHE_DIR}\""
+  echo "Please edit the CCACHE_DIR build variable or mount the directory."
+fi
+
+export LC_ALL="C"
