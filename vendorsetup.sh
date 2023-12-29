902e674e3f823ffc5fcfe37765276671a8cf8c4f
Rama Bondan Prakoso <ramanarubp@gmail.com>
2023-12-29 23:28:22 +0700

    mt6789-common: Patch to fix fastbootd
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -49,3 +49,15 @@ export LC_ALL="C"
 
 # Clone to fix build on minimal manifest
 git clone https://android.googlesource.com/platform/external/gflags/ -b android-12.1.0_r4 external/gflags
+
+# Patches
+RET=0
+cd bootable/recovery
+git apply ../../device/transsion/mt6789-common/patches/0001-twrp.cpp-Always-unmap-super-devices-on-fastbootd-mod.patch > /dev/null 2>&1 || RET=$?
+cd ../../
+if [ $RET -ne 0 ];then
+    echo "ERROR: Patch is not applied! Maybe it's already patched?"
+else
+    echo "OK: All patched"
+fi
+
