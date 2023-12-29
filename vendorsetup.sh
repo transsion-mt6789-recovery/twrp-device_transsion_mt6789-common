c95dc3557e74ca1e6b95f95c0a02097037323659
Shivelight <shivelight@tuta.io>
2023-12-29 23:35:52 +0700

    mt6789-common: Patch haptic activation file path
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -54,6 +54,7 @@ git clone https://android.googlesource.com/platform/external/gflags/ -b android-
 RET=0
 cd bootable/recovery
 git apply ../../device/transsion/mt6789-common/patches/0001-twrp.cpp-Always-unmap-super-devices-on-fastbootd-mod.patch > /dev/null 2>&1 || RET=$?
+git apply ../../device/transsion/mt6789-common/patches/0002-Change-haptics-activation-file-path.patch > /dev/null 2>&1 || RET=$?
 cd ../../
 if [ $RET -ne 0 ];then
     echo "ERROR: Patch is not applied! Maybe it's already patched?"
